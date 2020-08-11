a2b228cfdf46c6e876c40e596bdc56265a1d50e5
minhbka <minhbka@gmail.com>
2020-08-11 22:14:58 +0900

    add store and provider on App component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Provider } from "react-redux";
 import "./App.css";
 import CakeContainer from "./components/CakeContainer";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div className='App'>
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,5 @@
 import { createStore } from "redux";
 import cakeReducer from "./cake/cakeReducer";
-const store = createStore();
+const store = createStore(cakeReducer);
+
+export default store;
